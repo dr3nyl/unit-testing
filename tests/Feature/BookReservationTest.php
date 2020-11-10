a0a347aa857d47f27822342b1bc7c8b47484aecf
Drenyl <drenyltobi@gmail.com>
2020-11-10 13:05:51 +0800

    changes for book testing unit

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Book;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+    
+   /** @test */
+   public function a_book_can_be_added_to_the_library(){
+
+        $this->withoutExceptionHandling();
+        
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Victor'    
+        ]);
+
+        $response->assertOK();
+
+        $this->assertCount(1, Book::all());
+   }
+
+   /** @test */
+   public function a_title_is_required(){
+
+    //$this->withoutExceptionHandling();
+    
+    $response = $this->post('/books', [
+        'title' => '',
+        'author' => 'Victor'
+    ]);
+    
+    $response->assertSessionHasErrors('title');
+    
+}
+
+}
